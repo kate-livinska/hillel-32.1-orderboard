651e31df1e18a6611cbab098a3833a6bb63dd839
Kateryna Livinska <kateryna.livinska@gmail.com>
2025-01-24 15:45:53 +0100

    Add unit tests

---
+++ src/test/java/app/coffee/order/CoffeeOrderBoardTest.java
@@ -0,0 +1,102 @@
+package app.coffee.order;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static test.data.TestData.*;
+
+class CoffeeOrderBoardTest {
+    CoffeeOrderBoard coffeeOrderBoard;
+
+    @BeforeEach
+    void setUp() {
+        Queue<Order> orders = new LinkedList<>();
+        orders.add(ORDER1);
+        orders.add(ORDER2);
+        orders.add(ORDER3);
+        orders.add(ORDER4);
+
+        coffeeOrderBoard = new CoffeeOrderBoard(orders);
+    }
+
+    @Test
+    void addValidOrder_OK() {
+        Object[] expectedOrders = {ORDER1, ORDER2, ORDER3, ORDER4, ORDER5};
+
+        coffeeOrderBoard.add(ORDER5);
+        Object[] actualOrders = coffeeOrderBoard.getOrders().toArray();
+
+        assertArrayEquals(expectedOrders, actualOrders);
+    }
+
+    @Test
+    void addNullOrder() {
+        coffeeOrderBoard.add(null);
+        Object[] expectedOrders = {ORDER1, ORDER2, ORDER3, ORDER4};
+        assertArrayEquals(expectedOrders, coffeeOrderBoard.getOrders().toArray());
+    }
+
+    @Test
+    void deliver() {
+        Order actualOrder = coffeeOrderBoard.deliver();
+
+        assertEquals(ORDER1, actualOrder);
+    }
+
+    @Test
+    void deliver_specificOrderIsDelivered() {
+        Order actualOrder = coffeeOrderBoard.deliver(ORDER3.getOrderNumber());
+
+        assertEquals(ORDER3, actualOrder);
+    }
+
+    @Test
+    void deliver_specificOrderIsRemovedFromQueue() {
+        Queue<Order> expectedOrders = new LinkedList<>();
+        expectedOrders.add(ORDER1);
+        expectedOrders.add(ORDER2);
+        expectedOrders.add(ORDER4);
+
+        coffeeOrderBoard.deliver(ORDER3.getOrderNumber());
+
+        assertEquals(expectedOrders, coffeeOrderBoard.getOrders());
+    }
+
+    @Test
+    void deliverSpecificOrder_notInQueueReturnsNull() {
+        Order actualNullOrderOrder = coffeeOrderBoard.deliver(ORDER5.getOrderNumber());
+
+        assertNull(actualNullOrderOrder);
+    }
+
+    @Test
+    void draw() {
+        final PrintStream standardOut = System.out;
+        final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        coffeeOrderBoard.draw();
+
+        String expectedOutput = "Num | Name\n1 | Customer1\n2 | Customer2\n3 | Customer3\n4 | Customer4\n";
+
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void setOrders() {
+        Queue<Order> orders = new LinkedList<>();
+        orders.add(ORDER5);
+
+        coffeeOrderBoard.setOrders(orders);
+
+        assertEquals(orders, coffeeOrderBoard.getOrders());
+    }
+}
---
+++ src/test/java/test/data/TestData.java
@@ -0,0 +1,11 @@
+package test.data;
+
+import app.coffee.order.Order;
+
+public class TestData {
+    public static final Order ORDER1 = new Order("Customer1");
+    public static final Order ORDER2 = new Order("Customer2");
+    public static final Order ORDER3 = new Order("Customer3");
+    public static final Order ORDER4 = new Order("Customer4");
+    public static final Order ORDER5 = new Order("Customer5");
+}
